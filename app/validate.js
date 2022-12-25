f27a06a3c2ab9594db9cbbfa50e76948331c0e11
Alexandre Martins <alexandrevmartinsdelima@gmail.com>
2022-12-25 12:47:27 -0300

    acertando o numero secreto

--- app/validate.js
+++ app/validate.js
@@ -1,7 +1,27 @@
 function checkIfGuessHasValidValue(guess) {
 	const number = +guess;
 
-	if (Number.isNaN(number)) {
-		console.log('O valor inserido é inválido');
+	if (invalidGuess(number)) {
+		guessElement.innerHTML += `<div>Valor Inválido!</div>`;
 	}
+
+	if (numberGreaterOrLessThanAllowedValue(number)) {
+		guessElement.innerHTML += `<div>Valor inválido:
+			Fale um número entre <strong>${minValue}</strong> e <strong>${maxValue}</strong></div>`;
+	}
+
+	if (number === secretNumber) {
+		document.body.innerHTML = `
+			<h2>Você acertou!</h2>
+			<h3>O número secreto era ${secretNumber}.</h3>
+		`
+	}
+}
+
+function invalidGuess(number) {
+	return Number.isNaN(number);
+}
+
+function numberGreaterOrLessThanAllowedValue(number) {
+	return number > maxValue || number < minValue;
 }
