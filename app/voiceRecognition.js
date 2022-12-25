64120e6ebbb578a4d3fca4f831e636e644c96016
Alexandre Martins <alexandrevmartinsdelima@gmail.com>
2022-12-24 23:08:47 -0300

    feedback visual e iniciando validacao

--- app/voiceRecognition.js
+++ app/voiceRecognition.js
@@ -1,11 +1,25 @@
+const guessElement = document.querySelector('#guess')
+
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition()
-recognition.lang = 'pt-BR'
+recognition.lang = 'pt-br'
 recognition.start();
 
 recognition.addEventListener('result', onSpeak)
 
 function onSpeak(event) {
-    console.log(event.results[0][0].transcript);
+    const guess = event.results[0][0].transcript;
+    const filteredGuess = guess.replace(/[!?\.]/g, '')
+
+    showGuessInScreen(filteredGuess);
+    checkIfGuessHasValidValue(filteredGuess);
+}
+
+function showGuessInScreen(guess) {
+    guessElement.innerHTML = `
+        <div>Você disse:</div>
+
+        <span class="guess-box">${guess}</span>
+    `
 }
